d25edd1babe0612a4adc935af03cfc082d71206e
dattecode <dattecode@gmail.com>
2026-02-27 22:02:35 -0300

    templates-rendering-parte-1 leonardo-valenzuela

---
+++ alumnos/leonardo-valenzuela/templateAndRendering/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from "vue";
+import Card from "./components/Card.vue";
+
+const users = ref([]);
+
+const nuevoUsuario = ref("");
+
+const crearUsuario = () => {
+  users.value.push(nuevoUsuario.value);
+  nuevoUsuario.value = "";
+};
+</script>
+
+<template>
+  <h1>Nombre de usuario:</h1>
+  <input type="text" v-model="nuevoUsuario" />
+  <button @click="crearUsuario">Crear usuario</button>
+  <h2 v-if="users.length === 0">No hay usuarios</h2>
+  <section v-else class="cards">
+    <Card v-for="(user, index) in users" :key="index" :user="user" />
+  </section>
+</template>
+
+<style scoped>
+.cards{
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+</style>
---
+++ alumnos/leonardo-valenzuela/templateAndRendering/src/components/Card.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  user: String
+});
+
+</script>
+
+<template>
+  <div class="card">
+    <h4>Usuario:</h4>
+    <p >
+      {{ props.user }}
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.card{
+  background-color: lightblue;
+  padding: 1rem;
+  border-radius: 1rem;
+  margin: 1rem;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+}
+</style>
