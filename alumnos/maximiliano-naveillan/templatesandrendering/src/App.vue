2ef5af04ca359abd09d5275f2d34ba1a886ddf1d
MaximilianoNaveillan <maximilianonaveillan@gmail.com>
2026-02-27 21:46:51 -0300

    leccion templates and rendering maximiliano-naveillan

---
+++ alumnos/maximiliano-naveillan/templatesandrendering/src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from 'vue';
+import Card from './components/Card.vue';
+/* Eventos (JS).... */
+let msn = ref('Hola Vue.js 👌 ...');
+const cambiarMensaje = () => {
+  msn.value = 'Mensaje cambiado...';
+};
+
+let nombre = 'Maximiliano';
+let apellido = 'Naveillan';
+
+function saludar() {
+  console.log('Hola');
+}
+</script>
+
+<template>
+  <!-- HTML Estructuras declarativas dentro de bloques <template> -->
+  <Card :msn="msn" />
+  <div class="card">
+    <p>
+      <!-- Interpolación de datos -->
+      {{ 1 + 3 }}
+      {{ nombre + ' ' + apellido }}
+      {{ console.log('Hola mundo') }}
+      {{ saludar() }}
+    </p>
+    <button @click="cambiarMensaje()">Click me</button>
+  </div>
+</template>
+
+<style scoped>
+/*CSS ...*/
+</style>
