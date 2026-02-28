a577c03102f1b94a2de18cb37ca641c7bedac95a
mjvss <mariajose.valcarcel.s@gmail.com>
2026-02-27 23:25:38 -0300

    Agrega actividad Live Coding - María Jos é

---
+++ alumnos/maria-jose/live-coding-templateandrendering/src/App.vue
@@ -0,0 +1,29 @@
+<script setup>
+  import { ref } from 'vue';
+
+  let usuarios = ref([]);
+
+  let nuevoUsuario = ref("")
+
+  function crearUsuario (){
+    usuarios.value.push(nuevoUsuario.value)
+    nuevoUsuario.value = ""
+  }
+</script>
+
+<template>
+ <input type="text" v-model="nuevoUsuario"> 
+ <button @click="crearUsuario">Crear Usuario</button>
+<h1 v-if="usuarios.length === 0">
+  No hay usuarios
+</h1>
+<div v-else>
+ <h2 v-for="(user,index) in usuarios" :key="index">
+  {{ user }}
+ </h2>
+</div>
+</template>
+
+<style>
+
+</style>
