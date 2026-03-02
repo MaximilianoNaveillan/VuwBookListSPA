22234913d5de02776641dde384f8704fc5aab40c
mjvss <mariajose.valcarcel.s@gmail.com>
2026-03-02 20:44:12 -0300

    Template and rendering

--- alumnos/maria-jose/live-coding-templateandrendering/src/App.vue
+++ alumnos/maria-jose/live-coding-templateandrendering/src/App.vue
@@ -1,23 +1,35 @@
 <script setup>
+// Aquí se importa la referencia que usaremos más tarde
   import { ref } from 'vue';
 
+// Referencia le da el valor a un array, sirve para almacenar los datos del input (usuarios)
   let usuarios = ref([]);
 
+// Referencia a un string (valor de texto) para capturar el valor y almacenarlo en la referencia usuarios
   let nuevoUsuario = ref("")
 
+// Función que entrega el valor del string nuevoUsuario a usuarios a través de un push
+// Hay que hacer .value para entregar el valor a usuarios
   function crearUsuario (){
+    // console.log(nuevoUsuario)
     usuarios.value.push(nuevoUsuario.value)
     nuevoUsuario.value = ""
   }
 </script>
 
 <template>
- <input type="text" v-model="nuevoUsuario"> 
+<!-- Se crea un elemento para captar el valor ingresado. Se guarda en la referencia de nuevoUsuario con v-model -->
+ <input type="text" v-model="nuevoUsuario">
+<!-- Se crea botón que capta el click y ejecuta lo que está dentro de la función crearUsuario -->
  <button @click="crearUsuario">Crear Usuario</button>
+<!-- En el h1, creamos una condición de que si no hay valores ingresados en la referencia usuarios se imprime -->
 <h1 v-if="usuarios.length === 0">
   No hay usuarios
 </h1>
+<!-- Si hay usuario ingresado, se imprime el contenido -->
 <div v-else>
+
+  <!-- Se utiliza v-for para generar un listado de todos los usuarios ingresados -->
  <h2 v-for="(user,index) in usuarios" :key="index">
   {{ user }}
  </h2>
---
+++ alumnos/maria-jose/templatesandrendering/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue';
+import Card from './components/card.vue';
+/* Eventos (JS).... */
+let msn = ref('Hola Vue.js 👌 ...');
+const cambiarMensaje = () => {
+  msn.value = 'Mensaje cambiado...';
+};
+</script>
+
+<template>
+  <!-- HTML Estructuras declarativas dentro de bloques <template> -->
+  <h1>{{ msn }}</h1>
+  <p>Esto es una plantilla Vue.js</p>
+  <Card />
+  <p>
+    {{ 1 + 3 }}
+    <!-- Interpolación de datos -->
+  </p>
+  <button @click="cambiarMensaje()">Click me</button>
+</template>
+
+<style scoped>
+/*CSS ...*/
+</style>
